b72cbdcafbe2ed302deaa719fa435963e23ed14c
VarunKumarGampa <gampavarunkumar@gmail.com>
2023-10-06 15:11:29 +0530

    complete implenentation of login page

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+import '@babel/polyfill';
+import { login } from './login';
+
+//Dom elements
+const loginForm = document.querySelector('.form');
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //values
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -4,7 +4,8 @@ const viewController = require('./../controllers/viewsController')
 const authController = require('./../controllers/authController')
 
 
+router.use(authController.isLoggedIn)
 router.get('/', viewController.overview)
-router.get('/tour/:slug',viewController.gettour)
+router.get('/tour/:slug', viewController.gettour)
 router.get('/login',viewController.getLoginForm)
 module.exports = router
