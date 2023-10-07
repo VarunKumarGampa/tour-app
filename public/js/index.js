aaf4b72de5ec93aea8070e2c610464a33cd34e4b
VarunKumarGampa <gampavarunkumar@gmail.com>
2023-10-07 14:00:52 +0530

    updating user data using API

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 import '@babel/polyfill';
 import { login,logout } from './login';
+import {updateData} from './updateSettings'
 
 //Dom elements
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout')
+const userDataForm = document.querySelector('.form-user-data')
 
 if (loginForm) {
   loginForm.addEventListener('submit', (e) => {
@@ -17,4 +19,13 @@ if (loginForm) {
 
 if(logOutBtn){
   logOutBtn.addEventListener('click',logout)
+}
+
+if(userDataForm){
+  userDataForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name,email)
+  })
 }
